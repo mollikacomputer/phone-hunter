f32fdf551fa15b43e5052894b66be7bfd191b0a0
mollikacomputer <mollikacomputer3@gmail.com>
2022-03-01 18:03:02 +0600

    phone info updatd

--- js/app.js
+++ js/app.js
@@ -13,11 +13,11 @@ const searchPhone=()=>{
     fetch(url)
     .then(response=>response.json())
     .then(data=> showPhone(data.data))
+    
     }
 }
 
 const showPhone=(phones)=>{
-    
     // console.log(phones);
     const displayPhone = document.getElementById('display-phone-id');
     displayPhone.textContent = '';
@@ -28,14 +28,14 @@ const showPhone=(phones)=>{
         document.getElementById('spinner').style.display = 'none';
         document.getElementById('error-message').style.display = 'none';
         phones.forEach(phone=>{
-                console.log(phone);
+                // console.log(phone);
                 const div = document.createElement('div');
                 div.classList.add('col');
                 div.innerHTML = `
                 <div class="card h-100">
                     <img src="${phone.image}" class="card-img-top" alt="...">
                 <div class="card-body">
-                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <h5 class="card-title"> ${phone.phone_name}</h5>
                     <p class="card-text">${phone.brand}</p>
                 </div>
                 <div class="card-footer">
@@ -62,11 +62,16 @@ const showPhoneInfo = (info)=>{
         <div class="card" style="width: 80%;">
             <img src="${info.data.image}" class="card-img-top" alt="...">
         <div class="card-body">
-            <h5 class="card-title">${info.data.name} </h5>
-            <p class="card-text">${info.data.name} </p>
-            <p class="card-text">${info.data.releaseDate} </p>
-            <p class="card-text">${info.data.mainFeatures.storage} </p>
-            <p class="card-text">${info.data.others.WLAN} </p>
+            <h5 class="card-title"><b>Phone Name :</b> ${info.data.name} </h5>
+            <p class="card-text"><b>Phone Brand :</b> ${info.data.brand} </p>
+            <p class="card-text"><b>Realese Date :</b> ${info.data.release} </p>
+            
+            <p class="card-text"><b>Phone Release Date </b> 
+            <span id="release-data">
+                ${info.data.releaseDate} 
+             </span> </p>
+            <p class="card-text"><b>Phone Storage : </b>${info.data.mainFeatures.storage} </p>
+            <p class="card-text"> <b>Other info WLAN : </b>${info.data.others.WLAN} </p>
             
         </div>
     </div>
