e83c0e546b884b22dca71090f50e26c62a5ae02e
mollikacomputer <mollikacomputer3@gmail.com>
2022-03-01 17:22:31 +0600

    added spinner error message

--- js/app.js
+++ js/app.js
@@ -1,38 +1,52 @@
-document.getElementById('error-message').style.display='none';
+document.getElementById('spinner').style.display='none';
 const searchPhone=()=>{
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value.toLowerCase();
     searchField.value = '';
+    if(searchText ==''){
+        document.getElementById('spinner').style.display='block';
+        document.getElementById('error-message').style.display='block';
+        document.getElementById('display-phone-id').textContent = '';
+    }else{
     // console.log(searchText);
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     fetch(url)
     .then(response=>response.json())
     .then(data=> showPhone(data.data))
-
+    }
 }
 
 const showPhone=(phones)=>{
+    
     // console.log(phones);
     const displayPhone = document.getElementById('display-phone-id');
     displayPhone.textContent = '';
-    phones.forEach(phone=>{
-        // console.log(phone);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-            <div class="card h-100">
-                <img src="${phone.image}" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h5 class="card-title">${phone.phone_name}</h5>
-                <p class="card-text">${phone.brand}</p>
+    if(phones.length===0){
+        document.getElementById('spinner').style.display = 'block';
+        document.getElementById('error-message').style.display = 'block';
+    }else{
+        document.getElementById('spinner').style.display = 'none';
+        document.getElementById('error-message').style.display = 'none';
+        phones.forEach(phone=>{
+                console.log(phone);
+                const div = document.createElement('div');
+                div.classList.add('col');
+                div.innerHTML = `
+                <div class="card h-100">
+                    <img src="${phone.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${phone.phone_name}</h5>
+                    <p class="card-text">${phone.brand}</p>
+                </div>
+                <div class="card-footer">
+                <button onclick="phoneDetails('${phone.slug}')" type="button" class="btn btn-light bg-white">Show Details</button>
+                </div>
             </div>
-            <div class="card-footer">
-            <button onclick="phoneDetails('${phone.slug}')" type="button" class="btn btn-light bg-white">Show Details</button>
-            </div>
-        </div>
         `;
         displayPhone.appendChild(div);
     })
+    }
+
 }
 const phoneDetails=(id)=>{
     const url = `https://openapi.programming-hero.com/api/phone/${id}`;
@@ -42,8 +56,6 @@ const phoneDetails=(id)=>{
     .then(data=>showPhoneInfo(data))
 
 }
-
-
 
 const showPhoneInfo = (info)=>{
     document.getElementById('info-id').innerHTML = `
